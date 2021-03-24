6ccafa4a7eb39dc0e49672d0e82ba08c56f3026b
Diego Koscky <2228939+diegokoscky@users.noreply.github.com>
2021-03-24 15:39:18 -0300

    Troca do vídeo da hero

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css'
 function HeroSection() {
     return (
         <div className="hero-container">
-            <video src="/videos/video-2.mp4" autoPlay loop muted />
+            <video src="/videos/video-1.mp4" autoPlay loop muted />
             <h1>A Aventura Aguarda</h1>
             <p>O que você está esperando?</p>
             <div className="hero-btns">
