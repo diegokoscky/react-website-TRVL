3c7d2f3144f255228aa421b9b7eb236f96a256b6
Diego Koscky <2228939+diegokoscky@users.noreply.github.com>
2021-03-24 15:35:20 -0300

    Alterações no footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Button } from './Button'
 import './Footer.css'
 
@@ -27,11 +28,92 @@ function Footer() {
             </section>
             <div className="footer-links">
                 <div className="footer-link-wrapper">
-                    <div className="footer-links-item">
-                        
+                    <div className="footer-link-items">
+                        <h2>Sobre Nós</h2>
+                        <Link to="/sign-up">Como funciona</Link>
+                        <Link to="/">Depoimentos</Link>
+                        <Link to="/">Carreiras</Link>
+                        <Link to="/">Investidores</Link>
+                        <Link to="/">Termos de Serviço</Link>
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>Contato</h2>
+                        <Link to='/'>Contato</Link>
+                        <Link to='/'>Suporte</Link>
+                        <Link to='/'>Destinos</Link>
+                        <Link to='/'>Anunciantes</Link>
+                    </div>
+                </div>
+                <div className='footer-link-wrapper'>
+                    <div class='footer-link-items'>
+                        <h2>Videos</h2>
+                        <Link to='/'>Poste um vídeo</Link>
+                        <Link to='/'>Embaixadores</Link>
+                        <Link to='/'>Agência</Link>
+                        <Link to='/'>Influencers</Link>
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>Social Media</h2>
+                        <Link to='/'>Instagram</Link>
+                        <Link to='/'>Facebook</Link>
+                        <Link to='/'>Youtube</Link>
+                        <Link to='/'>Twitter</Link>
                     </div>
                 </div>
             </div>
+            <section class='social-media'>
+                <div class='social-media-wrap'>
+                    <div class='footer-logo'>
+                        <Link to='/' className='social-logo'>
+                        TRVL
+                        <i class='fab fa-typo3' />
+                        </Link>
+                    </div>
+                    <small class='website-rights'>TRVL © 2020</small>
+                    <div class='social-icons'>
+                        <Link
+                        class='social-icon-link facebook'
+                        to='/'
+                        target='_blank'
+                        aria-label='Facebook'
+                        >
+                        <i class='fab fa-facebook-f' />
+                        </Link>
+                        <Link
+                        class='social-icon-link instagram'
+                        to='/'
+                        target='_blank'
+                        aria-label='Instagram'
+                        >
+                        <i class='fab fa-instagram' />
+                        </Link>
+                        <Link
+                        class='social-icon-link youtube'
+                        to='/'
+                        target='_blank'
+                        aria-label='Youtube'
+                        >
+                        <i class='fab fa-youtube' />
+                        </Link>
+                        <Link
+                        class='social-icon-link twitter'
+                        to='/'
+                        target='_blank'
+                        aria-label='Twitter'
+                        >
+                        <i class='fab fa-twitter' />
+                        </Link>
+                        <Link
+                        class='social-icon-link twitter'
+                        to='/'
+                        target='_blank'
+                        aria-label='LinkedIn'
+                        >
+                        <i class='fab fa-linkedin' />
+                        </Link>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
